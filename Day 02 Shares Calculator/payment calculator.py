69c9374b582b699693c8003a29d9cccffe198999
Tee-001cyb <rolawal01@gmail.com>
2026-07-02 16:14:09 +0100

    Enhance tip input handling in payment calculator
    Added option to skip tip input and handle user input more robustly.

--- Day 02 Shares Calculator/payment calculator.py
+++ Day 02 Shares Calculator/payment calculator.py
@@ -1,7 +1,12 @@
 while True:  
     bill = float(input("How much is the bill:"))
-    tip = float(input("how many percent are you tipping 10, 20, 30,: "))
-    Tip = (bill /100 * tip)
+    tip = (input("how many percent are you tipping (10, 20, 30, or 'n': "))
+    if tip == "n":
+        Tip = 0
+        Print("No tip added")
+    else:
+        tip = float(tip)
+          Tip = (bill /100 * tip)
     total_amount = bill + Tip
     print(f"Here is your total bill including tips, {total_amount}")
     people = float(input("how many people are spilting the bills split to?: "))
