5a7b986119e6612afe798637b2e8666680bca609
Tee-001cyb <rolawal01@gmail.com>
2026-07-02 15:05:26 +0100

    Add Day 2

---
+++ Day 02 Shares Calculator/payment calculator.py
@@ -0,0 +1,12 @@
+while True:  
+    bill = float(input("How much is the bill:"))
+    tip = float(input("how many percent are you tipping 10, 20, 30,: "))
+    Tip = (bill /100 * tip)
+    total_amount = bill + Tip
+    print(f"Here is your total bill including tips, {total_amount}")
+    people = float(input("how many people are spilting the bills split to?: "))
+    payment_per_person = total_amount / people
+    print(f"Here is the payment for each person {payment_per_person}")
+    instruction = input("do you want to exit press 'y',(or 'n' to continue) ")
+    if instruction == 'y':
+         print("Calculation done")
